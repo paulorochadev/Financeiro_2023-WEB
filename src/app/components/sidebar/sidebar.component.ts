71f9ba1141526543460d10cd15ce94eacf3d8f5b
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-20 13:53:55 -0300

    feat: Cria método selectMenu para o SideBar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,4 +10,34 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
+  /**
+   *
+   */
+  constructor(private router : Router) {
+    
+  }
+
+
+  selectMenu(menu: Number) {
+    switch(menu) {
+      case 1:
+        this.router.navigate(['/dashboard']);
+        break;
+      
+      case 2:
+        this.router.navigate(['/sistema']);
+        break;
+      
+      case 3:
+        this.router.navigate(['/categoria']);
+        break;
+      
+      case 4:
+        this.router.navigate(['/despesa']);
+        break;
+      
+      default:
+        break;
+    }
+  }
 }
