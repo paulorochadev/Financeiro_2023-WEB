2450b243f92485f8bbc970ae69f08ecdf01572a5
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-20 14:39:17 -0300

    chore: Cria Components Categoria, Despesa e Sistema

---
+++ src/app/pages/despesa/despesa.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-despesa',
+  standalone: true,
+  imports: [],
+  templateUrl: './despesa.component.html',
+  styleUrl: './despesa.component.scss'
+})
+export class DespesaComponent {
+
+}
