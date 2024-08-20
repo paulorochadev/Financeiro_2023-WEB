227298af629806d810570bf76ada00d89f7d2887
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-20 14:48:51 -0300

    chore: Cria Rotas de acesso as Telas-Paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+import { CategoriaComponent } from './pages/categoria/categoria.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DespesaComponent } from './pages/despesa/despesa.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SistemaComponent } from './pages/sistema/sistema.component';
 
 export const routes: Routes = [
     {
@@ -19,5 +22,17 @@ export const routes: Routes = [
     {
         path: 'dashboard',
         component: DashboardComponent
+    },
+    {
+        path: 'sistema',
+        component: SistemaComponent
+    },
+    {
+        path: 'categoria',
+        component: CategoriaComponent
+    },
+    {
+        path: 'despesa',
+        component: DespesaComponent
     }
 ];
