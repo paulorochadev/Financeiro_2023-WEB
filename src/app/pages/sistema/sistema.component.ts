fdafd6d3e84464eda3d67e798386fa4b90e84ed2
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-20 18:55:32 -0300

    feat: Cria e configura Form da Tela de Cadastro de Sistemas

--- src/app/pages/sistema/sistema.component.ts
+++ src/app/pages/sistema/sistema.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { MenuService } from '../../services/menu.service';
@@ -6,7 +8,7 @@ import { MenuService } from '../../services/menu.service';
 @Component({
   selector: 'app-sistema',
   standalone: true,
-  imports: [ NavbarComponent, SidebarComponent ],
+  imports: [ CommonModule, NavbarComponent, SidebarComponent, ReactiveFormsModule ],
   templateUrl: './sistema.component.html',
   styleUrl: './sistema.component.scss'
 })
@@ -15,11 +17,28 @@ export class SistemaComponent {
   /**
    *
    */
-  constructor(public menuService: MenuService) {
+  constructor(public menuService: MenuService, public formBuilder: FormBuilder) {
     
   }
 
+  sistemaForm: FormGroup;
+
   ngOnInit(): void {
     this.menuService.menuSelecionado = 2;
+
+    this.sistemaForm = this.formBuilder.group({
+      name: ['', [Validators.required]]
+    })
+  }
+
+  dadosForm() {
+    return this.sistemaForm.controls;
+  }
+
+  enviar() {
+    debugger
+    var dados = this.dadosForm();
+
+    alert(dados["name"].value)
   }
 }
