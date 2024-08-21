19e1a203fb7bc31a497bbfaf6db1350421049776
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2024-08-21 12:14:01 -0300

    feat: Cria estrutura HTML e SCSS da Tela de Cadastro de Categorias

--- src/app/pages/categoria/categoria.component.ts
+++ src/app/pages/categoria/categoria.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 import { MenuService } from '../../services/menu.service';
@@ -6,7 +8,7 @@ import { MenuService } from '../../services/menu.service';
 @Component({
   selector: 'app-categoria',
   standalone: true,
-  imports: [ NavbarComponent, SidebarComponent ],
+  imports: [ CommonModule, NavbarComponent, SidebarComponent, ReactiveFormsModule ],
   templateUrl: './categoria.component.html',
   styleUrl: './categoria.component.scss'
 })
@@ -15,11 +17,28 @@ export class CategoriaComponent {
   /**
    *
    */
-  constructor(public menuService: MenuService) {
+  constructor(public menuService: MenuService, public formBuilder: FormBuilder) {
     
   }
 
+  categoriaForm: FormGroup;
+
   ngOnInit(): void {
     this.menuService.menuSelecionado = 3;
+
+    this.categoriaForm = this.formBuilder.group({
+      name: ['', [Validators.required]]
+    })
+  }
+
+  dadosForm() {
+    return this.categoriaForm.controls;
+  }
+
+  enviar() {
+    debugger
+    var dados = this.dadosForm();
+
+    alert(dados["name"].value)
   }
 }
